525e3cad014f30ecf142747792cf7b077222a715
Timothy Gilbert <tsgilbert35@hotmail.com>
2025-07-28 18:04:27 -0700

    adding UI to app, shinyJS, colors

--- WedScraping-App/server.R
+++ WedScraping-App/server.R
@@ -1,11 +1,24 @@
 function(input, output, session) {
-  
+
   res_status <- reactiveVal("Waiting for input...")
+  res_data <- reactiveVal(NULL)
   
   observeEvent(input$go_button, {
-    
     res_status("Checking URL...")
     
+    # Insert new UI after the original
+    insertUI(
+      selector = "#ui_initial",
+      where = "afterBegin",
+      ui = tags$div(id = "new_ui", class = "new-ui",
+                    textInput("parse_key", label = "What node do you want to parse?", placeholder = "href, text, node"),
+                    actionButton("go_button2", label = "Evaluate New URL"),
+      )
+    )
+    
+    # Hide the original UI
+    hide("ui_initial")
+
     url_input <- input$url
     go <- TRUE
     message <- ""
@@ -30,15 +43,81 @@ function(input, output, session) {
           pause_cap = 5,
           terminate_on = c(404)
         )
+        res_data(res)  # save for downstream use
         res_status(paste("Status Code:", status_code(res)))
       }, error = function(e) {
         res_status(paste("Request failed:", e$message))
+        res_data(NULL)
       })
+      
+      # if valid - make box green -->
+      if (res_status() == "Status Code: 200") {
+        # Insert new UI after the original
+        insertUI(
+          selector = "#box-1",
+          where = "afterEnd",
+          ui = tags$div(
+            br(),
+            div(id="box-1-goButtonPush-TRUE",
+                tags$div(class = "output-box-1-goButtonPush-TRUE",
+                         h4("URL Valid!"),
+                         textOutput("url_2"),
+                         br(),
+                         actionButton("ok", "  Get HTML", width = "100%"))),
+          )
+        )
+        # Hide the original UI
+        hide("box-1")
+      } else {
+        # if not valid - make box red -->
+        insertUI(
+          selector = "#box-1",
+          where = "afterEnd",
+          ui = tags$div(
+            br(),
+            div(id="box-1-goButtonPush-FALSE",
+                tags$div(class = "output-box-1-goButtonPush-FALSE",
+                         h4("URL Not Valid!"),
+                         textOutput("url_2"),
+                         br(),
+                         actionButton("reset", "  Reset", width = "100%"))),
+          )
+        )
+        # Hide the original UI
+        hide("box-1")
+      } # end of else statement
+      
+      res_status()
     }
     
+    # for UI status output -->
+    output$url <- renderText({
+      res_status()
+    })
+    output$url_2 <- renderText({
+      res_status()
+    })
+    
+    # output$raw_html <- renderText({
+    #   req(res_data())  # ensures it's non-NULL
+    #   page <- read_html(res_data())
+    #   as.character(page)
+    # })
+    
+  }) # end of go button
+  
+  observeEvent(input$go_button2, {
+    req(res_data())
+    page <- read_html(res_data())
+    
+    # Find all player name nodes
+    names <- page %>%
+      html_nodes(".name") %>%
+      html_text(trim = TRUE)
+    
+    output$parsed_names <- renderPrint({
+      names
+    })
   })
   
-  output$url <- renderText({
-    res_status()
-  })
 }
--- WedScraping-App/ui.R
+++ WedScraping-App/ui.R
@@ -3,27 +3,50 @@
 library(shiny)
 library(rvest)
 library(httr)
+library(shinyjs)
 
 # end of global settings
 
 fluidPage(
+  includeCSS("www/styles.css"),
+  useShinyjs(),
 
-    titlePanel(" "),
-
+    titlePanel("Interactive Web Scraper"),
+  
     sidebarLayout(
-        sidebarPanel(
-            shiny::textInput("url",
-                             label = "Website URL",
-                             value = "https://",
+      div(id="ui_initial",
+        sidebarPanel(width = "100%",
+          tags$div(class = "sidebar-panel",
+            textInput("url",
+                             label = "⬇️ Enter Website Below ⬇️",
+                             value = "https://247sports.com/college/arizona/Season/2026-Football/Commits/",
                              placeholder = "https://247sports.com/college/arizona/Season/2026-Football/Commits/"),
             actionButton("go_button",
                          label = "Evaluate URL")
-        ),
+          ))
+        ), # end of div for UI
         
-
+      
+      
+        # main pane view -->
         mainPanel(
-            textOutput("url"),
-            textOutput('url_valid')
-        )
+
+          tags$div(class = "flex-box",
+
+            div(id="box-1",
+            tags$div(class = "output-box-1",
+              h4("⬅️Check if URL is Valid..."),
+              textOutput("url")
+              )),
+
+            # div(id="box-2",
+            # tags$div(class = "output-box-2",
+            #   h4("HTML"),
+            #   verbatimTextOutput("raw_html")
+            #   ))
+            
+            ) # end of tags$div
+        ) # end of main panel view
+      
     )
 )
